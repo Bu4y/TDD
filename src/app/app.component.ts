5a81950f70a4b31ace12704c118c18449ca5ce10
Bu4y <amnes1a@windowslive.com>
2017-08-15 18:00:57 +0700

    new project start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { ProfilePage } from "../pages/profile/profile";
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = HomePage;
+  rootPage: any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
